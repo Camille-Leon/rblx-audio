6c64bb458174501e5e9736f739357626a06bd009
Camille <99615794+Camille-Leon@users.noreply.github.com>
2023-05-10 18:48:13 -0700

    Update audio.lua

--- audio.lua
+++ audio.lua
@@ -1,1 +1,20 @@
+local audio = {sounds = {}}
 
+for i, child in ipairs(script:GetChildren()) do -- let's loop this shit
+	if child:IsA("Sound") then
+		audio.sounds[child.Name] = child -- shove the children into the container (don't arrest me, i am just a programmer)
+	end
+end
+
+warn(audio.sounds["Footstep"])
+
+function audio.PlaySound(soundName)
+	local sound = audio.sounds[soundName]
+	if sound then
+		sound:Play()
+	else
+		warn("Sound not found:", soundName) -- BRO
+	end
+end
+
+return audio
